3766e5ab4551fa2b5780b0b851a20ee43c677e9c
mangokitten <katrine.jordheim@gmail.com>
2016-03-09 21:21:20 +0100

    Fix url patterns for rutinator

--- apps/rutinator/dashboard/urls.py
+++ apps/rutinator/dashboard/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from apps.rutinator.dashboard.views import TaskListView, CreateTaskView, EditTaskView
 
-urlpatterns = patterns(
-    'apps.rutinator.dashboard.views',
+urlpatterns = [
     url(r'^$', TaskListView.as_view(), name='task_view'),
     url(r'^create/$', CreateTaskView.as_view(), name='task_create'),
     url(r'^edit/(?P<pk>\d+)/$', EditTaskView.as_view(), name='task_edit')
-)
-
+]
