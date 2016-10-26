a712228af5be3fa45d3101fee0730dd7ae6a14a1
junglebooboo <aigtum@gmail.com>
2016-10-26 20:40:56 +0200

    Rutinator sends email with a reminder Removed octal representation from apps/events - looked weird

--- apps/rutinator/mommy.py
+++ apps/rutinator/mommy.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import locale
 import logging
 
-from django.conf import settings
-from django.core.mail import EmailMessage
 from django.utils import timezone
 
-from .models import Task
-from .views import send_email
+from .models import Task as rutinatorTask
+from .send_mail import send_email
 from apps.mommy import schedule
 from apps.mommy.registry import Task
+from .signals import new_membership_approval_handler
 
 
 class RutinatorMail(Task):
@@ -18,11 +16,10 @@ class RutinatorMail(Task):
     def run():
         logger = logging.getLogger("rutinator")
         logger.info("Rutinator job started")
-        locale.setlocale(locale.LC_ALL, "nb_NO.UTF-8")
-        rutinator_tasks = Task.objects.filter(deadline__gte=timezone.now())
+        rutinator_tasks = rutinatorTask.objects.filter(deadline__gte=timezone.now())
 
         for task in rutinator_tasks:
             send_email(task)
 
 
-schedule.register(RutinatorMail, day_of_week='mon-sun', hour=22, minute=50)
+schedule.register(RutinatorMail, day_of_week='mon-sun', hour=8, minute=0)
